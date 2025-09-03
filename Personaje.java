82ce7bb19aa3ee492ec49a02faadb48f222dc1e1
AKSAN01 <santivargas222@gmail.com>
2025-09-03 09:41:27 -0500

    harrypotter

---
+++ Personaje.java
@@ -0,0 +1,23 @@
+
+public abstract class Personaje {
+    protected String nombre;
+    protected Hechizo hechizoHabil; 
+
+    public Personaje(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
+    public void lanzarHechizo() {
+        System.out.print(nombre + " lanza: ");
+        hechizoHabil.ejecutar();
+    }
+
+
+    public void setHechizoHabil(Hechizo nuevoHechizo) {
+        this.hechizoHabil = nuevoHechizo;
+        System.out.println("\n>> " + nombre + " ha cambiado su hechizo principal. <<\n");
+    }
+    
+    public abstract void presentarse();
+}
